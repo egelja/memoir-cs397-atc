c5e57255ed0ae12c89be3e779f737e5cb102b9a5
Nikola Maruszewski <nikolamaruszewski2026@u.northwestern.edu>
2024-05-23 17:16:47 -0500

    unrolling in add test

--- tests/unit/sequence_add_vector/main.cpp
+++ tests/unit/sequence_add_vector/main.cpp
@@ -2,23 +2,47 @@
 
 #include "cmemoir/cmemoir.h"
 
-#define SEQLEN 10
+#define SEQLEN 16 // must be multiple of 4 lol
+
+#define DO_ADD(d, s1, s2, i)                                                   \
+  memoir_index_write(                                                          \
+      u64,                                                                     \
+      memoir_index_read(u64, s1, i) + memoir_index_read(u64, s2, i),           \
+      d,                                                                       \
+      i)
 
 using namespace memoir;
 
-void add(memoir::Collection *seq1,
-         memoir::Collection *seq2,
-         memoir::Collection *seq3) {
-
-  for (size_t i = 0; i < SEQLEN; ++i) {
-    memoir_index_write(
-        u64,
-        memoir_index_read(u64, seq1, i) + memoir_index_read(u64, seq2, i),
-        seq3,
-        i);
+void add_loop(memoir::Collection *seq1,
+              memoir::Collection *seq2,
+              memoir::Collection *seq3) {
+  for (size_t i = 0; i < SEQLEN; i += 4) {
+    // manually unrolled
+    DO_ADD(seq3, seq1, seq2, i);
+    DO_ADD(seq3, seq1, seq2, i + 1);
+    DO_ADD(seq3, seq1, seq2, i + 2);
+    DO_ADD(seq3, seq1, seq2, i + 3);
   }
 
   return;
+}
+
+void add_4(memoir::Collection *seq1,
+           memoir::Collection *seq2,
+           memoir::Collection *seq3) {
+  DO_ADD(seq3, seq1, seq2, 0);
+  DO_ADD(seq3, seq1, seq2, 1);
+  DO_ADD(seq3, seq1, seq2, 2);
+  DO_ADD(seq3, seq1, seq2, 3);
+}
+
+void print_seq(memoir::Collection *seq) {
+  printf(" Result: \n");
+  for (auto i = 0; i < SEQLEN; i++) {
+    auto read = memoir_index_read(u64, seq, i);
+    printf("%02lu, ", read);
+  }
+  printf("\n");
 }
 
 int main(int argc, char *argv[]) {
@@ -33,16 +57,17 @@ int main(int argc, char *argv[]) {
     memoir_index_write(u64, i * 10, seq2, i);
   }
 
-  printf("\nAdding\n");
+  // add first 4
+  printf("\nAdding first 4...");
 
-  add(seq1, seq2, seq3);
+  add_4(seq1, seq2, seq3);
+  print_seq(seq3);
 
-  printf("\nResult: \n");
-  for (auto i = 0; i < SEQLEN; i++) {
-    auto read = memoir_index_read(u64, seq3, i);
-    printf("%02lu, ", read);
-  }
-  printf("\n");
+  // add in a loop
+  printf("\nAdding in a loop...");
+
+  add_loop(seq1, seq2, seq3);
+  print_seq(seq3);
 
   return 0;
 }
