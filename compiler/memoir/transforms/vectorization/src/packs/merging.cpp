71304f1bfae21c6ff586e8559c217532b06538a8
Nikola Maruszewski <nikolamaruszewski2026@u.northwestern.edu>
2024-05-25 17:27:08 -0500

    get ready to implement merging

---
+++ compiler/memoir/transforms/vectorization/src/packs/merging.cpp
@@ -0,0 +1,13 @@
+#include "merging.hpp"
+
+PackSet
+merge_packs(PackSet ps)
+{
+    // Fixed point algo
+    PackSet ps_prev;
+
+    do {
+    } while (ps != ps_prev);
+
+    return ps;
+}
---
+++ compiler/memoir/transforms/vectorization/src/packs/merging.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "pack_set.hpp"
+
+/**
+ * Combine backs together into "vector packs".
+ */
+PackSet merge_packs(PackSet ps);
