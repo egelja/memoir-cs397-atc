84899faca1a1345883e811cd153a792332a95e47
Ryan <rewong03@gmail.com>
2024-05-26 21:26:49 -0500

    refactoring

--- compiler/memoir/transforms/vectorization/src/packs/extension.hpp
+++ compiler/memoir/transforms/vectorization/src/packs/extension.hpp
@@ -1,10 +1,34 @@
 #pragma once
 
+#include "pack.hpp"
 #include "pack_set.hpp"
 
 #include "llvm/IR/BasicBlock.h"
+#include "noelle/core/Noelle.hpp"
 
-/**
- * Extend a pack set by following use-def and def-use chains.
- */
-PackSet extend_packset(llvm::BasicBlock* BB, PackSet ps);
+using namespace arcana::noelle;
+
+class PacksetExtender {
+    // Class for building out a packset from an initial seeded pack
+    PDG* fdg;
+
+    std::unordered_set<llvm::Instruction*> free_left_instrs;
+    std::unordered_set<llvm::Instruction*> free_right_instrs;
+    PackSet* pack_set;
+
+public:
+    PacksetExtender(llvm::BasicBlock& bb, PackSet* p_set, PDG* graph);
+
+    void extend();
+
+private:
+    bool is_isomorphic(llvm::Instruction* instr_1, llvm::Instruction* instr_2);
+
+    bool is_independent(llvm::Instruction* instr_1, llvm::Instruction* instr_2);
+
+    bool instrs_can_pack(llvm::Instruction* instr_1, llvm::Instruction* instr_2);
+
+    bool follow_use_defs(Pack p);
+
+    bool follow_def_uses(Pack p);
+};
